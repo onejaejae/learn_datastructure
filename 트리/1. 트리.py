c8227bbfae05500932ed842c0142c7a393267327
Cho Won Jae <yoteamo7@naver.com>
2022-05-16 21:19:56 +0900

    트리 #1

---
+++ 트리/1. 트리.py
@@ -0,0 +1,32 @@
+# 파이썬 객체지향 프로그래밍으로 링크드리스트 구현하기
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+class NodeMgmt:
+    def __init__(self, head):
+        self.head = head
+    
+    def insert(self, value):
+        self.current_node = self.head
+        while True:
+            if self.current_node.value > value:
+                if self.current_node.left != None:
+                    self.current_node = self.current_node.left
+                else:
+                    self.current_node.left = Node(value)
+                    break
+            else:
+                if self.current_node.right != None:
+                    self.current_node = self.current_node.right
+                else:
+                    self.current_node.right = Node(value)
+                    break
+
+
+head = Node(1)
+BST = NodeMgmt(head)
+BST.insert(2)

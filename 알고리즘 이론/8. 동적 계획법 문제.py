77bba760eff40b4c1330e8fa3903f41c1a078597
Cho Won Jae <yoteamo7@naver.com>
2022-05-30 12:57:39 +0900

    동적 계획법 문제 #1

---
+++ 알고리즘 이론/8. 동적 계획법 문제.py
@@ -0,0 +1,13 @@
+def dp(num):
+    cache = [0 for i in range(num+1)]
+    
+    cache[1] = 1
+    cache[2] = 2
+
+    for i in range(3, num+1):
+        cache[i] = cache[i-1] + cache[i-2]
+
+    return cache[num] % 10007
+
+num = int(input())
+print(dp(num))

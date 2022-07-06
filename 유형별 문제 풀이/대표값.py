ddb5a0630f49cd81b31225649f8b941bca9bef0c
Cho Won Jae <yoteamo7@naver.com>
2022-07-06 14:46:56 +0900

    대표값

---
+++ 유형별 문제 풀이/대표값.py
@@ -0,0 +1,14 @@
+#import sys
+#sys.stdin = open("C:\workspaces\자료구조\유형별 문제 풀이\input.txt", "rt")
+
+n = int(input())
+score = list(map(int, input().split()))
+
+math_avg = round(sum(score) / n)
+
+for i, data in enumerate(score):
+    score[i] = (i, data, abs(data-math_avg))
+
+#https://gorokke.tistory.com/38
+score = sorted(score, key=lambda x: (x[2], -x[1], x[0]))
+print(math_avg, score[0][0] +1)

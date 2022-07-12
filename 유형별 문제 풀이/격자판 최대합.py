67073e21ae1e6da1126651327d49b35c3d667262
Cho Won Jae <yoteamo7@naver.com>
2022-07-12 14:19:30 +0900

    격자판 최대합

---
+++ 유형별 문제 풀이/격자판 최대합.py
@@ -0,0 +1,29 @@
+import sys
+sys.stdin = open("C:\workspaces\자료구조\유형별 문제 풀이\input.txt", "rt")
+
+n = int(input())
+array = [list(map(int, input().split())) for _ in range(n)]
+largest = -2160000
+
+for i in range(n):
+    sum1 = sum2 = 0
+    for j in range(n):
+        # 행의 합
+        sum1 += array[i][j]
+        # 열의 합
+        sum2 += array[j][i]
+    if largest < sum1:
+        largest = sum1
+    if largest < sum2:
+        largest = sum2 
+
+sum1=sum2=0
+for i in range(n):
+    sum1 += array[i][i]
+    sum2 += array[i][n-1-i]
+    if largest < sum1:
+        largest = sum1
+    if largest < sum2:
+        largest = sum2 
+
+print(largest)

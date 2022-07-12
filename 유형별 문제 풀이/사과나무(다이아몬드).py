988ac1c1fbd57939946f0348788b334afb13b9a9
Cho Won Jae <yoteamo7@naver.com>
2022-07-12 14:58:41 +0900

    사과나무(다이아몬)

---
+++ 유형별 문제 풀이/사과나무(다이아몬드).py
@@ -0,0 +1,20 @@
+# import sys
+# sys.stdin = open("C:\workspaces\자료구조\유형별 문제 풀이\input.txt", "rt")
+
+n = int(input())
+array = [list(map(int, input().split())) for _ in range(n)]
+p1 = p2 = n // 2
+total = array[0][p1]
+
+for i in range(1, n):
+    if i > n // 2:
+        p1 += 1
+        p2 -= 1
+        for a in range(p1, p2+1):
+            total += array[i][a]
+    else:
+        p1 -= 1
+        p2 += 1
+        for b in range(p1, p2+1):
+            total += array[i][b]
+print(total)

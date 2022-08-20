00c26bcddf3307e566994d67b08dbbad8df696d5
Cho Won Jae <yoteamo7@naver.com>
2022-08-20 21:05:57 +0900

    순열 추측하기

--- 11번가/2.py
+++ 11번가/2.py
@@ -1,7 +1,1 @@
-def sum(a,b):
-    return a+b
-
-add = lambda a,b : a+b
-
-print(sum(10,20))
-print(add(10,20))
+print([1,2] - [3,5])
--- 프로그래머스/21. 음양 더하기.py
+++ 프로그래머스/21. 음양 더하기.py
@@ -1,11 +1,7 @@
 def solution(absolutes, signs):
-    answer = 0
-
     for i in range(len(signs)):
         if signs[i] == False:
             absolutes[i] *= -1
-
-    print(sum(absolutes))
-    return answer
+    return sum(absolutes)
 
 print(solution([4, 7, 12], [True, False, True]))

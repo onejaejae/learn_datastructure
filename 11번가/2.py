d0ecf722ae1d02d6c49cb27156abbc0471704e33
Cho Won Jae <yoteamo7@naver.com>
2022-08-24 20:11:51 +0900

    3진법 뒤집기

--- 11번가/2.py
+++ 11번가/2.py
@@ -1,1 +1,7 @@
-print([1,2] - [3,5])
+def print_hello():
+    hello = 'Hello, world!'
+    def print_message():
+        print(hello)
+    print_message()
+ 
+print_hello()
---
+++ 프로그래머스/32. 3진법 뒤집기.py
@@ -0,0 +1,16 @@
+def solution(n):
+    res = []
+    sum = 0
+    j=0
+
+    while n > 0:
+        res.append(n % 3)
+        n //= 3
+
+    for i in range(len(res)-1,-1,-1):
+        if res[i] != 0:
+            sum += (pow(3,j) * res[i])
+        j+=1
+        
+    return sum
+print(solution(125))
